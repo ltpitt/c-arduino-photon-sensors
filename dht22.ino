15f7de98bd4cdf88a1d4b4cfbbb81074d01e12d7
Davide Nastri <d.nastri@gmail.com>
2016-07-16 23:11:16 +0200

    feat: implemented checkService function
    This function allows to check if REST API is available before proceeding

--- dht22.ino
+++ dht22.ino
@@ -1,142 +1,180 @@
-// Acquire and publish temperature and humidity from DHT22 sensor using Particle Photon
-//
-// To read variables from Particle Api use:
-// https://api.particle.io/v1/devices/YOUR_DEVICE_ID/humidity/?access_token=YOUR_ACCESS_TOKEN
-//
-// Davide Nastri, 11/2015
+#include "HttpClient/HttpClient.h"
+#include "InternetButton/InternetButton.h"
 
-#include "PietteTech_DHT/PietteTech_DHT.h"
-#include "math.h"
+/**
+* Declaring the variables.
+*/
+unsigned int nextTime = 0;    // Next time to contact the server
 
-#define DHTTYPE  DHT22       // Sensor type DHT11/21/22/AM2301/AM2302
-#define DHTPIN   D2           // Digital pin for communications
+HttpClient http;
+// Headers currently need to be set at init, useful for API keys etc.
+http_header_t headers[] = {
+    //  { "Content-Type", "application/json" },
+    //  { "Accept" , "application/json" },
+    { "Accept" , "*/*"},
+    { NULL, NULL } // NOTE: Always terminate headers will NULL
+};
 
-// declaration
-void dht_wrapper(); // must be declared before the lib initialization
+http_request_t request;
+http_response_t response;
+InternetButton b = InternetButton();
+int i;
 
-// library instialization
-PietteTech_DHT DHT(DHTPIN, DHTTYPE, dht_wrapper);
-
-int n;      // sensor acquisition counter
-double temp;  // temperature  
-double umid;  // humidity
-double t;   // needed in rounding
-
-void dht_wrapper() {
-    DHT.isrCallback();
-}
-
-
-
-void setup()
-{
+void setup() {
     Serial.begin(9600);
-
-    Serial.println("Device starting...");
-
-    Serial.println("DHT Example program using DHT.acquireAndWait");
-    Serial.print("LIB version: ");
-    Serial.println(DHTLIB_VERSION);
-    Serial.println("---------------");
-    
-    pinMode(D7, OUTPUT);
-
-    Particle.variable("humidity", umid); //write the value on the cloud variable called "Humidity"
-    Particle.variable("temperature", temp); //write the value on the cloud variable called "Temperature"
-
+    b.begin();
+    b.allLedsOff();
+    RGB.control(true); 
+    RGB.color(0, 0, 0);
 }
 
+void loop() {
 
-void loop()
-{
-    Serial.print("\n");
-    Serial.print(n);
-    Serial.print(": Retrieving information from sensor: ");
-    Serial.print("Read sensor: ");
-    delay(100);
-
-    int result = DHT.acquireAndWait();
-
-    
-    // Error handling
-    switch (result) {
-        case DHTLIB_OK:
-            Serial.println("OK");
-            break;
-        case DHTLIB_ERROR_CHECKSUM:
-            Serial.println("Error\n\r\tChecksum error");
-            break;
-        case DHTLIB_ERROR_ISR_TIMEOUT:
-            Serial.println("Error\n\r\tISR time out error");
-            break;
-        case DHTLIB_ERROR_RESPONSE_TIMEOUT:
-            Serial.println("Error\n\r\tResponse time out error");
-            break;
-        case DHTLIB_ERROR_DATA_TIMEOUT:
-            Serial.println("Error\n\r\tData time out error");
-            break;
-        case DHTLIB_ERROR_ACQUIRING:
-            Serial.println("Error\n\r\tAcquiring");
-            break;
-        case DHTLIB_ERROR_DELTA:
-            Serial.println("Error\n\r\tDelta time to small");
-            break;
-        case DHTLIB_ERROR_NOTSTARTED:
-            Serial.println("Error\n\r\tNot started");
-            break;
-        default:
-            Serial.println("Unknown error");
-            break;
+    if(b.buttonOn(1)){
+        b.ledOn(1, 15, 0, 0);
+        b.ledOn(11, 15, 0, 0);
+        checkService(1);
+    }
+     
+    if(b.buttonOn(2)){
+        b.ledOn(3, 15, 0, 0);
+        checkService(2);
     }
 
-
-    // humidity rounding
-    umid=DHT.getHumidity();
-    t=umid-floor(umid);  // get only decimal part
-    t=t*100; // multiply by 100 decimal part, get xx.xxxxxxxxx
-    t=floor(t); // get only last two characters of decimal part, get yy
-    umid=floor(umid)+(t/100);
-
-
-    // temperature rounding
-    temp=DHT.getCelsius();
-    t=temp-floor(temp); // get only decimal part
-    t=t*100; // multiply by 100 decimal part, get xx.xxxxxxxxx
-    t=floor(t); // get only last two characters of decimal part, get yy
-    temp=floor(temp)+(t/100);
-
-
-    // print sensor data to serial
-    Serial.print("Humidity (%): ");
-    Serial.println(DHT.getHumidity(), 2);
-
-    Serial.print("Temperature (oC): ");
-    Serial.println(DHT.getCelsius(), 2);
-
-    Serial.print("Temperature (oF): ");
-    Serial.println(DHT.getFahrenheit(), 2);
-
-    Serial.print("Temperature (K): ");
-    Serial.println(DHT.getKelvin(), 2);
-
-    Serial.print("Dew Point (oC): ");
-    Serial.println(DHT.getDewPoint());
-
-    Serial.print("Dew Point Slow (oC): ");
-    Serial.println(DHT.getDewPointSlow());
-
+    if(b.buttonOn(3)){
+        b.ledOn(6, 15, 0, 0);
+        checkService(3);
+    }
     
-    // Blink onboard led
-    digitalWrite(D7, HIGH);
-    delay(1000);
-    digitalWrite(D7, LOW);
+    if(b.buttonOn(4)){
+        b.ledOn(9, 15, 0, 0);
+        checkService(4);
+    }
 
-
-    // Increase acquisition counter
-    n++;
-    
-
-    // Wait for 10 minutes
-    delay(600000);
 }
 
+void okLed(int led){
+    
+    if (led == 1) {
+            b.ledOn(1, 0, 15, 0);
+            b.ledOn(11, 0, 15, 0);
+            delay(1000);
+            b.ledOff(1);
+            b.ledOff(11);
+    }
+    else {
+        b.ledOn(led, 0, 15, 0);
+        delay(1000);
+        b.ledOff(led);
+    }
+}
+
+void koLed(){
+    for (i = 0; i < 3; i++) {
+        b.allLedsOn(15, 0, 0);
+        delay(500);
+        b.allLedsOff();
+        delay(500);
+    }
+    
+}
+
+String restCall(int button){
+    
+    static const uint16_t TIMEOUT = 95000; // Allow maximum 95s between data packets.
+
+    request.hostname = "YOURSERVERIP";
+    request.port = YOURSERVERPORT;
+
+    // Check if user already requested the restCall less than 2 seconds ago
+    if (nextTime > millis()) {
+        koLed();
+        return "Nothing done: button pressed too soon after the previous request";
+    }
+
+    // Specified paths are just customizable examples
+    if (button == 1){
+        request.path = "/internet_button_1/1/unreal";
+    }
+    if (button == 2){
+        request.path = "/internet_button_1/2/unreal";
+    }
+    if (button == 3){
+        request.path = "/internet_button_1/3/unreal";
+    }
+    if (button == 4){
+        request.path = "/internet_button_1/4/unreal";
+    }
+    
+    // Get request
+    http.get(request, response, headers);
+
+    // Print response.status and response.body to serial
+    Serial.println(response.status);
+    Serial.println(response.body);
+
+    // Update nextTime execution
+    nextTime = millis() + 500;
+
+    // If request went well blink led to inform user
+    // Response status and body can be customized to match
+    // The ones given by your REST API
+    if (response.status==200 and response.body=="done") {
+        
+        if (button==1){
+            okLed(1);
+        }
+        
+        if (button==2){
+            okLed(3);
+        }
+
+        
+        if (button==3){
+            okLed(6);
+        }
+        
+        if (button==4){
+            okLed(9);
+        }
+        
+    }
+    
+    if (response.status=-1 and button==1){
+ 
+        okLed(1);
+        
+    }
+    
+    if (response.status=-1 and button==2){
+        
+        okLed(3);
+    
+    }
+    
+    if (response.status=-1 and button==4){
+        
+        okLed(9);
+           
+    }
+
+    return response.body;
+    
+}
+
+String checkService(int button) {
+
+    request.hostname = "YOURSERVERIP";
+    request.port = YOURPORT;
+    request.path = "/ota";
+    // Get request
+    http.get(request, response, headers);
+
+    if (response.status==200) {
+        b.playSong("C1,4,C1,4,C1,4");
+        restCall(button);
+    } else {
+        koLed();
+    }
+
+}
